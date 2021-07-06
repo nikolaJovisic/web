fc7bf68569498174c7e57b174183c7fcd85628e7
ra74-2018 <nikolajovisic4@gmail.com>
2021-07-06 20:25:44 +0200

    Prikazuju se sakupljeni bodovi za kupce.

--- static/components/sviKorisnici.js
+++ static/components/sviKorisnici.js
@@ -2,65 +2,68 @@ Vue.component('sviKorisnici', {
 	data: function() {
 		return {
 			logged: false,
-			role : window.localStorage.getItem("role"),
+			role: window.localStorage.getItem("role"),
 			korisnici: null,
 			ascending: false,
 			sortColumn: '',
 			ulogaFilter: 'Kupac',
-			columns: [{ name: "korisnickoIme" }, { name: "ime" }, { name: "prezime" }]
+			columns: [{ name: "korisnickoIme" }, { name: "ime" }, { name: "prezime" }, {name: "sakupljeniBodovi"}]
 		}
 	},
 	computed: {
-		filtriraniKorisnici: function () {
+		filtriraniKorisnici: function() {
 			if (this.korisnici == null) return null;
 			searchTerm = this.ulogaFilter
-			return this.korisnici.filter(function(row){
+			return this.korisnici.filter(function(row) {
 				uloga = row.uloga
-				return uloga.includes(searchTerm)        
+				return uloga.includes(searchTerm)
 			});
-	}
+		}
 	},
-	
+
 	methods: {
 
 		"sortTable": function sortTable(col) {
 			if (this.sortColumn === col) {
 				this.ascending = !this.ascending;
-			  } else {
+			} else {
 				this.ascending = true;
 				this.sortColumn = col;
-			  }
-		
-			  var ascending = this.ascending;
+			}
+
+			var ascending = this.ascending;
 			this.korisnici.sort(function(a, b) {
 				if (a[col] > b[col]) {
-				  return ascending ? 1 : -1
+					return ascending ? 1 : -1
 				} else if (a[col] < b[col]) {
-				  return ascending ? -1 : 1
+					return ascending ? -1 : 1
 				}
 				return 0;
-			  })
-		  }
-		
-		
-	},
-	
-	mounted (){
-
-			axios.get("/sviKorisnici",{
-			headers: {
-			  },
-			  contentType:"application/json",
-			dataType:"json",
-			  })
-			.then(response => {
-				if(response.data)
-				{ 
-					this.korisnici = response.data;
-				}
 			})
 		},
 		
+		renderSakupljeniBodovi: function(colName) {
+			return this.ulogaFilter === 'Kupac' || colName !== 'sakupljeniBodovi';
+		}
+
+
+	},
+
+	mounted() {
+
+		axios.get("/sviKorisnici", {
+			headers: {
+			},
+			contentType: "application/json",
+			dataType: "json",
+		})
+			.then(response => {
+				if (response.data) {
+					this.korisnici = response.data;
+				}
+			})
+	},
+
 	template: `
 	<div>
 		<div>
@@ -77,14 +80,18 @@ Vue.component('sviKorisnici', {
 		 <thead>
 		   <tr>
 			<th v-for="col in columns" v-on:click="sortTable(col.name)"> 
-				{{col.name}} 
+				<div v-if="renderSakupljeniBodovi(col.name)">
+					{{col.name}}
+				</div> 
 		   </th>
 		   </tr>
 		 </thead>
 		 <tbody>
 		   <tr v-for="korisnik in filtriraniKorisnici">
 		   	<td v-for="col in columns">
-			   {{ korisnik[col.name] }}
+				<div v-if="renderSakupljeniBodovi(col.name)">
+			   		{{ korisnik[col.name] }}
+				</div> 
 			</td>
 		   </tr>
 		 </tbody>
